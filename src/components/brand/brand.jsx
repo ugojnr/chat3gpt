bde42d6af97b66c236f412fe7ee5711aae54b99b
Daniel Ofo-muogbo <Danielofomuogbo@yahoo.com>
2024-01-14 23:23:06 +0100

    update

--- src/components/brand/brand.jsx
+++ src/components/brand/brand.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from './import'
 
 const Brand = () => {
   return (
