6a94563591631867ca116376130397df002b1950
Daniel Ofo-muogbo <Danielofomuogbo@yahoo.com>
2024-03-11 20:07:34 +0100

    new update

--- src/containers/features/features.jsx
+++ src/containers/features/features.jsx
@@ -4,20 +4,20 @@ import "./features.css";
 
 const features = [
   {
-    title: "Improve your searches with tesera",
-    text: "get more information from your search",
+    title: "News Monitoring",
+    text: "Tesera offers end-to-end news monitoring and media intelligence across online news, social media, print and broadcast. Our news monitoring tool tracks keywords connected to the topics that matter to you and aggregates social media and editorial content that mentions your keywords.",
   },
   {
-    title: "Improve your searches with tesera",
-    text: "get more information from your search",
+    title: "Consume insights",
+    text: "With consumer preferences and markets evolving at breakneck speeds, businesses need a way to keep ahead of trends and understand what potential customers want and need. We apply AI, data science, and human market research expertise to a comprehensive social data stream that uncovers consumer insights for data-driven decision-making.",
   },
   {
-    title: "Improve your searches with tesera",
-    text: "get more information from your search",
+    title: "Social Media Engagement",
+    text: "Our scalable social media engagement tool helps you stay in control by providing real-time social media listening, analytics reporting, engagement and community management. With Tesera’s social media tools, you can be sure that you never miss a mention or respond to timely events and clarifications.",
   },
   {
-    title: "Improve your searches with tesera",
-    text: "get more information from your search",
+    title: "Public Relations Management",
+    text: "Our PR management tool enables PR professionals to stay on top of crisis communication, be in control of brand reputation, manage their media relations, discover influencers, pitch journalists and distribute content while employing generative AI assistance in PR reporting",
   },
 ];
 
--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -8,14 +8,13 @@ const Header = () => {
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
-          Welcome to Social Insights Uncover Trends Analytics
+          The AI-powered communications assistant. Across media, social and
+          consumer data.
         </h1>
         <p>
-          At Social Insights, we offer a cutting-edge platform that empowers
-          individuals and businesses to delve deep into the vast realms of
-          Twitter and Instagram data. Our intuitive and powerful analytics tools
-          enable you to extract valuable insights, track trends, and understand
-          audience sentiments like never before.
+          Tesera empowers communications, marketing and PR professionals with
+          strategic insights that inform better decision-making. Supercharging
+          government entities, leading enterprises and startups
         </p>
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your Email" />
